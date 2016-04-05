416a8cec346364b0630efb7d8be126123ffef2aa
Tension <admin@striverbar.com>
2016-04-05 20:12:38 +0800

    更新脚本

--- javascripts/Tencent404.js
+++ javascripts/Tencent404.js
@@ -1,2 +1,1 @@
-
-document.write('<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="/" homePageName="回到主页"></script>')
+document.write('<script type="text/javascript" src="http://www.qq.com/404/search_children.js?edition=small" charset="utf-8" homePageUrl="/" homePageName="回到主页"></script>')
